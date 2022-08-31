07bf33028e3335ff7c674c02667585ec393270eb
profecasale <112002132+profecasale@users.noreply.github.com>
2022-08-31 12:47:47 -0300

    Add files via upload

---
+++ pryCasaleSP1/frmMain.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace pryCasaleSP1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAcceder = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAcceder
+            // 
+            this.btnAcceder.Font = new System.Drawing.Font("Arial Black", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAcceder.Location = new System.Drawing.Point(50, 133);
+            this.btnAcceder.Name = "btnAcceder";
+            this.btnAcceder.Size = new System.Drawing.Size(386, 111);
+            this.btnAcceder.TabIndex = 0;
+            this.btnAcceder.Text = "Ingresar al Sistema";
+            this.btnAcceder.UseVisualStyleBackColor = true;
+            this.btnAcceder.Click += new System.EventHandler(this.btnAcceder_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 386);
+            this.Controls.Add(this.btnAcceder);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bienvenido al Sistema - SP1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAcceder;
+    }
+}
+
---
+++ pryCasaleSP1/frmMain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryCasaleSP1
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAcceder_Click(object sender, EventArgs e)
+        {
+            //frmLogin miFormulario ---> declarar un objeto del molde (clase) frmLogin
+            frmLogin miFormulario = new frmLogin();
+            //= new frmLogin(); --->crear en memoria el formulario para ser utilizado
+
+            //mostrar el formulario en pantalla
+            miFormulario.ShowDialog();
+        }
+    }
+}
